7e15541e588f4bc251fb8eb7e04a2e59a9e9fc2a
expensiveflower <nk0518ssss@gmail.com>
2020-11-16 17:06:19 +0800

    1. 載入網頁時先抓當下的 url，在透過上一頁按鈕去抓到當前頁面的數字 index 2. 新增文章到資料庫的地方把檢查文章是否重複寫好 3. 新增文章回覆內容到資料庫，改由文章的回覆內容來檢查是否有重複的回覆內容在資料庫裡面

--- ptt_baseabll.py
+++ ptt_baseabll.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 import time
 from datetime import datetime
+import re
 
 host = 'localhost'
 dbname = 'PTT_baseball'
@@ -15,18 +16,35 @@ print("資料庫連線成功!")
 db = client[dbname]
 
 # 選定資料表
-article_collection = db.article
-response_collection = db.response
+article_collection = db.article_1
+response_collection = db.response_1
 
-
-start_index = 10106
 url = "https://www.ptt.cc/bbs/Baseball/"
-pages = 1
+pages = 2
 
-def get_post_url(url):
+def get_post_url(target_url):
+
+	# 先抓首頁的連結最後面的 index 
+	html_doc = requests.get(target_url)
+	home_url = html_doc.url
+
+	init_soup = BeautifulSoup(html_doc.text, 'lxml')
+
+	# 直接抓首頁連結是沒有數字 index 的，所以要透過上一頁的連結來抓取數字 index
+	right_up_corner = init_soup.find_all('a', class_ = 'btn wide')
+	for url in right_up_corner:
+		# print(url.get_text(strip = True))
+		if url.get_text(strip = True) == "‹ 上頁":
+			# get the home page's path
+			home_url = url.get("href")
+	
+	# 因為抓到的數字是上一頁的，所以要加1加回來
+	start_index = int(re.search('(\d+).*', home_url).group(1)) + 1
+
+	PTT_baseball_domain = "https://www.ptt.cc/bbs/Baseball/"
 	for i in range(0, pages):
-		# 網頁網址
-		target_url = url + 'index' + str(start_index - i) + '.html'
+		
+		target_url = PTT_baseball_domain + 'index' + str(start_index - i) + '.html'
 		# print('網頁連結是 : ', target_url)
 
 		web = requests.get(target_url)
@@ -54,17 +72,26 @@ def get_post_info(url):
 	
 	post_info = soup.find_all("div", class_ = "article-metaline")
 
-	author_title_time_push['author'] = post_info[0].find("span", class_ = "article-meta-value").get_text(strip = True)
-	author_title_time_push['title'] = post_info[1].find("span", class_ = "article-meta-value").get_text(strip = True)
+	# print(post_info)
+
+	# 有些文章沒有作者、標題、日期的，就跳過不要跑
+	try:
+		author_title_time_push['author'] = post_info[0].find("span", class_ = "article-meta-value").get_text(strip = True)
+	except:
+		return(0)
+	try:
+		author_title_time_push['title'] = post_info[1].find("span", class_ = "article-meta-value").get_text(strip = True)
+	except:
+		return(0)
 	try:
 		author_title_time_push['post_time'] = datetime.strptime(post_info[2].find("span", class_ = "article-meta-value").get_text(strip = True), '%a %b %d %H:%M:%S %Y') 
 	except:
-		pass
+		return(0)
 	
 			
 	# 抓出全部留言，並宣告算噓推序的變數	
 	reply_list = soup.find_all("div", class_ = "push")
-	print("留言數 : ", len(reply_list))
+	# print("留言數 : ", len(reply_list))
 	score = 0
 	push_count = 0
 	abstract_count = 0
@@ -91,7 +118,7 @@ def get_post_info(url):
 	value = list(post_info_dict.values())[0]
 
 	post_id = insert_post(key, value)
-	# get_post_replies(post_id, soup)
+	get_post_replies(post_id, soup)
 
 
 def insert_post(post_url, post_info):
@@ -120,12 +147,8 @@ def get_post_replies(post_id, soup):
 
 	results = []
 
-	# print(reply_list)
-
 	if reply_list:
 		for reply in reply_list:
-
-			# print("回覆~~~" ,reply)
 		
 			result = {'article_id':post_id}
 
@@ -139,21 +162,26 @@ def get_post_replies(post_id, soup):
 			# print("一篇文的全部留言~~~~", results)
 
 		# 傳一個 list 過去 insert_responses()
-		# insert_responses(results)
+		insert_responses(results)
 
 def insert_responses(responses_list):
+	# 把每一則的回覆內容抓出來跟資料庫裡面的資料做比對
+	for reply in responses_list:
+		doc = response_collection.find_one({'reply_content':reply['reply_content']})
+		# 如果該筆回覆不在資料庫裡面就新增
+		if not doc:
+			reply_id = response_collection.insert_one(reply).inserted_id
+		# 如果存在則更新該筆資料的內容
+		else:
+			response_collection.update_one(
+				{'_id': reply['reply_id']},
+				{'$set':reply},
+				upsert = True
+			)
+			reply_id = reply['reply_id']
 
+	print("文章回覆新增成功!")
 
-	doc = response_collection.find_one({'article_id':responses_list[0]['article_id']})
-
-	if not doc:
-		for response_dict in responses_list:
-			reply_id = response_collection.insert_one(response_dict).inserted_id
-	else:
-		for response_dict in responses_list:
-			response_collection.update_many(
-				{'$set': response_dict},
-				upsert=True)
 
 
 if __name__ == "__main__":
